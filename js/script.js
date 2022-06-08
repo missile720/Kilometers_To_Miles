57ff00139116462a9b0e93eef31df73339142ce1
Misael Bolainez <misaelbolainez@gmail.com>
2022-06-08 10:28:51 -0700

    fix(js): added text to the output

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ document.querySelector('#addButton').onclick = function(){converter()};
 function converter(){
   kilometers = Number(document.getElementById("inputText").value);
   miles = kilometers/1.609344;
-  document.querySelector("#result").innerHTML = miles;
+  document.querySelector("#result").innerHTML = miles + " miles";
 }
